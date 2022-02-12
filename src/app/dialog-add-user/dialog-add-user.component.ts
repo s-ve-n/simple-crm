6712688c786cd638e548953dcc3feb2e0f41707e
plus <sven.radelof@gmail.com>
2022-02-12 23:45:53 +0100

    update

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
+import { User } from 'src/models/user.class';
 
 @Component({
   selector: 'app-dialog-add-user',
   templateUrl: './dialog-add-user.component.html',
-  styleUrls: ['./dialog-add-user.component.scss']
+  styleUrls: ['./dialog-add-user.component.scss'],
 })
 export class DialogAddUserComponent implements OnInit {
+  user = new User();
+  birthDate!: Date;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  saveUser() {
+    this.user.birthDate = this.birthDate.getTime();
+    console.log('Current user is', this.user);
   }
-
 }
